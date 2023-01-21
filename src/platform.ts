e926c72ffd3e6f3df569325e2472608f6eb27179
Amman Vedi <av@Ammans-MacBook-Pro.local>
2023-01-21 20:16:13 +0000

    fixes and add picture mode

--- src/platform.ts
+++ src/platform.ts
@@ -2,7 +2,7 @@
 
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import { PLUGIN_NAME } from './settings';
 import { TVAccessory } from './platformAccessory';
 
 
